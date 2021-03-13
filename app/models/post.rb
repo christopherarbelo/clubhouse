a562b5538bd500a9006ff9a0eb1204acdea88fd6
Christopher Arbelo <christopherarbelo@gmail.com>
2021-03-13 17:56:10 -0500

    Implement associations and only show username when logged in
    PostsController creates a post based on the association with the user. Index and post pages only username when logged in and edit and destroy commands are only available to the correct user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   validates :message, presence: true, length: { in: 1..200 }
+  belongs_to :user
+  validates_associated :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true, presence: true, length: { in: 3..13 }
   validates :name, uniqueness: true, presence: true, length: { in: 2..15 }
+  has_many :posts, dependent: :destroy
 end
